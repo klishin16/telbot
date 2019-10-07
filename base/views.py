2059891cb85f815ca75a4bffd0b8a380701077ba
klishin16 <klishin.nd16@gmail.com>
2019-10-07 23:06:00 +0300

    Update

--- base/views.py
+++ base/views.py
@@ -10,4 +10,4 @@ class PersonsList(ListView):
     template_name="persons.html"
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index. Update!")
